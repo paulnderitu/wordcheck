64e5349fa2d1285525b02aa2b0a4e3126be09f8f
Your Name <youremail@domain.com>
2017-07-03 17:47:33 +0300

    added logic for splitting the sentence into an array of words

--- spec/sentence_check_spec.rb
+++ spec/sentence_check_spec.rb
@@ -5,7 +5,8 @@ describe('sentence_check') do
   it("returns the inputted sentence") do
     expect(("boda boda should be owned by all boda boda riders").sentence_check).to eq(true)
   end
-  # it ("splits the sentence into single words in the sentence")do
-  #   expect(("boda boda should be owned by all boda boda riders").sentence_check).to eq("boda", "boda", "should", "be", "owned", "by", "all", "boda", "boda", "riders")
-  # end
+
+it "takes a sentence and splits it into an array" do
+    expect("boda boda should be owned by all boda boda riders".split).to(eq(["boda", "boda", "should", "be", "owned", "by", "all", "boda", "boda", "riders"]))
+  end
 end
