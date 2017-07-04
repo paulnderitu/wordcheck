1ff710a05027d6af4ca4310c95dad8e6c66135f1
Your Name <youremail@domain.com>
2017-07-04 07:19:55 +0300

    refactored the code to make it DRY

--- lib/sentence_check.rb
+++ lib/sentence_check.rb
@@ -1,5 +1,13 @@
 class String
-define_method(:sentence_check) do
-  self == self
-  end
+  define_method(:count) do |word|
+    self == self
+    sentence = self.split()
+    counter = 0
+    sentence.each() do |list|
+      if list == word
+          counter = counter + 1
+      end
+      end
+  counter
+    end
 end
